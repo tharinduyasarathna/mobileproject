67270cb739107581d5537a8aa078f48e608e2fd3
Lakshan Karunathilake <manualsplash@gmail.com>
2019-07-08 23:40:41 +0530

    Inject notification service

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { LocalNotifications } from "@ionic-native/local-notifications/ngx";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class NotificationService {
+  constructor(private notifications: LocalNotifications) {}
 
-  constructor() { }
+  /**
+   * This method can be used to schedule a notification
+   */
+  scheduleANotification = () => {
+    this.notifications.schedule({
+      id: 1,
+      text: "Single ILocalNotification",
+      sound: isAndroid ? "file://sound.mp3" : "file://beep.caf",
+      data: { secret: key }
+    });
+  };
 }
