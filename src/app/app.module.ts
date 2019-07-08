d809ddc3de29e41e94de1d9cd881d20eb33e2eeb
Lakshan Karunathilake <manualsplash@gmail.com>
2019-07-08 23:54:29 +0530

    Format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,45 @@
-import { ApiService } from '../app/services/api.service';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
- 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
- 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { environment } from 'src/environments/environment';
-import { AuthenticateService } from './services/authentication.service';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import { ApiService } from "../app/services/api.service";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
- 
-import * as firebase from 'firebase';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NotificationsComponent } from './components/notifications/notifications.component';
-import { NotificationService } from './services/notification/notification.service';
- 
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { environment } from "src/environments/environment";
+import { AuthenticateService } from "./services/authentication.service";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import * as firebase from "firebase";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NotificationsComponent } from "./components/notifications/notifications.component";
+import { NotificationService } from "./services/notification/notification.service";
+
 firebase.initializeApp(environment.firebase);
- 
+
 @NgModule({
-  declarations: [AppComponent,NotificationsComponent],
+  declarations: [AppComponent, NotificationsComponent],
   entryComponents: [NotificationsComponent],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    
+    BrowserModule,
+    IonicModule.forRoot(),
+
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AngularFireAuthModule,
-    
+    AngularFireAuthModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
     AuthenticateService,
     ApiService,
-    NotificationService
+    NotificationService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
